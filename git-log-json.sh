aa77c29d31f05570468abf281deee4e301738f97
Arthur Chiu <me@artchiu.org>
2025-09-30 17:32:58 +0800

    Fix JSON parsing errors in git-log-json.sh by escaping quotes in commit messages

--- git-log-json.sh
+++ git-log-json.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
-c="`git log --no-merges --pretty=format:'{%n  "sha": "%H",%n  "html_url": "https://github.com/yhchiu/ZeroRAM-Suspender/commit/%H",%n  "commit": {%n    "message": "%s",%n    "author": {%n      "name": "%aN",%n      "date": "%aI"%n    }%n  }%n},' | head -c -1`" ; echo -e "[\n$c\n]" > CHANGELOG.json
+git log --no-merges --format='%H|%s|%aN|%aI' | \
+awk -F'|' '
+BEGIN { print "[" }
+{
+  gsub(/"/, "\\\"", $2)
+  gsub(/"/, "\\\"", $3)
+  if (NR > 1) print ","
+  printf "{\n"
+  printf "  \"sha\": \"%s\",\n", $1
+  printf "  \"html_url\": \"https://github.com/yhchiu/ZeroRAM-Suspender/commit/%s\",\n", $1
+  printf "  \"commit\": {\n"
+  printf "    \"message\": \"%s\",\n", $2
+  printf "    \"author\": {\n"
+  printf "      \"name\": \"%s\",\n", $3
+  printf "      \"date\": \"%s\"\n", $4
+  printf "    }\n"
+  printf "  }\n"
+  printf "}"
+}
+END { print "\n]" }
+' > CHANGELOG.json
 
 
